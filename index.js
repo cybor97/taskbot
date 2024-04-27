475802e93566814e7e3e4f884c992bcfb6349751
cyborg97 <muhametshin.1997@gmail.com>
2024-04-27 02:05:47 +0200

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply("Welcome!", {
+    reply_markup: {
+        inline_keyboard: [[{
+            text: "Open",
+            web_app: {
+                url: process.env.WEB_APP_URL,
+            }
+        }]]
+    }
+}));
+bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
